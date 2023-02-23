fa1f591b4fdfdccd5048c58506864946ce20bdd0
fesiib <tlekbay.b@gmail.com>
2023-02-23 21:57:59 +0900

    Prettier

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ const App = observer(function App() {
     return (
         <div className="App">
             <h1 className="text-3xl font-bold underline">Hello !</h1>
-			<div className="grid m-10 grid-cols-3 grid-rows-3">
-				<EditorCanvas 
-					className={"col-start-2 col-span-2 row-span-2 overflow-hidden"}
-					uiStore={uiStore}
-					domainStore={domainStore} 
-				/>
-			</div>
+            <div className="grid m-10 grid-cols-3 grid-rows-3">
+                <EditorCanvas
+                    className={"col-start-2 col-span-2 row-span-2 overflow-hidden"}
+                    uiStore={uiStore}
+                    domainStore={domainStore}
+                />
+            </div>
         </div>
     );
 });
--- src/stores/uiStore.js
+++ src/stores/uiStore.js
@@ -1,9 +1,6 @@
 import { makeAutoObservable } from "mobx";
 
-const ZOOM_PERCENTAGES = [
-	15, 30, 50, 80, 100,
-	125, 150, 200, 300, 400, 500
-];
+const ZOOM_PERCENTAGES = [15, 30, 50, 80, 100, 125, 150, 200, 300, 400, 500];
 
 class UIStore {
     // Session Info
@@ -15,13 +12,13 @@ class UIStore {
     };
 
     canvasSize = {
-        width: window.innerWidth / 3 * 2,
-        height: window.innerHeight / 3 * 2,
-	};
+        width: (window.innerWidth / 3) * 2,
+        height: (window.innerHeight / 3) * 2,
+    };
 
-	canvasControls = {
-		zoom: 4,
-	}
+    canvasControls = {
+        zoom: 4,
+    };
     // timelineSize = {};
     // panelSize = {};
 
@@ -30,16 +27,16 @@ class UIStore {
         this.rootStore = rootStore;
     }
 
-	get zoomPercentage() {
-		return ZOOM_PERCENTAGES[this.canvasControls.zoom];
-	}
+    get zoomPercentage() {
+        return ZOOM_PERCENTAGES[this.canvasControls.zoom];
+    }
 
     setWindowSize({ width, height }) {
         this.windowSize = { width, height };
-		this.canvasSize = {
-			width: width / 3 * 2,
-			height: height / 3 * 2,
-		};
+        this.canvasSize = {
+            width: (width / 3) * 2,
+            height: (height / 3) * 2,
+        };
     }
 }
 
